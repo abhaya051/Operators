52ef7cac31d54ab735b0aea011916634dd65cdbc
Suraj Gautam <surajgautam783792@gmail.com>
2024-02-09 13:06:50 +0530

    Operators

---
+++ index.js
@@ -0,0 +1,191 @@
+// Arthmetic Operators
+
+//  Here a and b is called operands and between operators and all process is called expression
+// Addition---
+// let a = 52;
+// let b= 52;
+// console.log(a+b)
+
+
+// Subtraction
+// let a = 52;
+// let b= 69;
+// console.log(a-b)
+
+// Multiplication
+// let a = 52;
+// let b= 69;
+// console.log(a*b)
+
+// Expontetion
+// let a = 2;
+// let b= 6;
+// console.log(a**b)
+
+// Divison
+// let a = 2;
+// let b= 6;
+// console.log(a/b)
+
+// Modulus
+// let a = 6;
+// let b= 2;
+// console.log(a%b)
+
+
+// Below This Is Also Called Unary Operators
+
+// // Increment
+// let a = 52;
+// // console.log(++a)    // (a=a+1)This Means a++ Pre
+//    console.log(a++)                 // Post Increment
+//    console.log(a)
+
+
+
+// // Decrement 
+// let b = 52;
+// // console.log(--a)    // (a=a+1)This Means a++ Pre  
+// console.log(a--)                  // Post Decrement
+// console.log(a)
+
+
+//------------------------------------------------------------------------------
+
+
+
+
+
+
+// Assignment Operators Use To Assign Value
+// --------------------------------(=)
+// let a = 52;
+// console.log(a)
+
+
+// --------------------------------(+=)
+// let a = 52;
+// a += 56; // a = a + 56
+// console.log(a)
+
+// --------------------------------(-=)
+
+// let a = 52;
+// a -= 56; // a = a - 56
+// console.log(a)
+
+
+// --------------------------------(*=)
+
+// let a = 52;
+// a *= 56; // a = a * 56
+// console.log(a)
+
+
+// // --------------------------------(/=)
+
+// let a = 52;
+// a /= 56; // a = a / 56
+// console.log(a)
+
+// // --------------------------------(%=)
+
+// let a = 4;
+// a %= 2; // a = a % 56
+// console.log(a)
+
+// // --------------------------------(**=)
+
+// let a = 2;
+// a **= 6; // a = a * 56
+// console.log(a)
+
+
+
+
+
+// ---------------------------------------------------------------------------
+// Comparison Operators
+
+
+// Equal To 
+// let a = 58;
+// let b = 58;
+// console.log(a==b)
+
+// --------------------------->
+
+//  Not Equal
+// let a = 55;
+// let b = 58;
+// console.log(a!=b)
+
+// --------------------------->
+
+//  Equal Value And Equal Type
+// let a = 58;
+// let b = 58;
+// console.log(a===b)
+
+
+// //  Not Equal Value And Not Equal Type
+// let a = 59;
+// let b = 58;
+// console.log(a!==b)
+
+
+// Greater Than
+// let a = 59;
+// let b = 58;
+// console.log(a>b)
+
+// Less Than
+// let a = 59;
+// let b = 95;
+// console.log(a<b)
+
+// Greater Or Equal To
+// let a = 59;
+// let b = 59;
+// console.log(a>=b)
+
+
+// Less Or Equal To
+// let a = 96;
+// let b = 59;
+// console.log(a<=b)
+
+
+// Ternary Operators
+// let a = 96;
+// let b = 59;
+// console.log(a,b)
+
+
+// ---------------------------------------------------------------------------
+// Logical Operators (Give True And False)
+
+// LOGICAL AND (Dono He True  Give Kre )
+// let a = 69;
+// let b = 25;
+
+// let cod1 = a > b;
+// let cod2 = a < b;
+// console.log(cod1 && cod2)
+// ------------------------------------->>>>>>>>>>>>
+
+// LOGICAL OR (Dono Mai Sai Koi Ek True It Give True )
+// let a = 69;
+// let b = 69;
+
+// let cod1 = a >= b;
+// let cod2 = a < b;
+// console.log(cod1 || cod2)
+
+// ------------------------------------->>>>>>>>>>>>
+
+// LOGICAL NOT (Dono Mai Sai Koi Ek True It Give True )
+// let a = 69;
+// let b = 56;
+
+// console.log(!(a==b)) //True
